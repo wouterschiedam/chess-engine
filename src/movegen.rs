9be79bd3ddaf80467abba348c7306f9d3dcacd00
Wouterschiedam <wouterschiedam@hotmail.com>
2024-04-17 17:34:01 +0200

    fix: tests

--- src/movegen.rs
+++ src/movegen.rs
@@ -118,6 +118,7 @@ impl MoveGenerator {
         let bb_opponent_pieces = board.bb_side[board.side_to_not_move()];
         let mut bb_pieces = board.get_pieces(piece, player);
         // Generate all moves for each piece
+
         while bb_pieces > 0 {
             let from = bits::next(&mut bb_pieces);
             let bb_target = match piece {
@@ -360,54 +361,70 @@ mod tests {
 
     use super::*;
 
+    // Reference:
+    // https://www.chessprogramming.org/Perft_Results
     #[test]
     fn depth_nodes() {
-        let mut board = Board::new();
-        // let _ = board.read_fen(Some(
-        //     "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-        // ));
-        //
-        let _ = board.read_fen(Some("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1"));
+        let positions = vec![
+            "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+            "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
+            "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1",
+            // "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
+            // "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
+            // "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
+        ];
 
-        let move_generator = MoveGenerator::new();
-        let mut move_list = MoveList::new();
+        for position in positions {
+            let mut board = Board::new();
+            let _ = board.read_fen(Some(position));
 
-        let mut total_nodes: i32 = 0;
+            let move_generator = MoveGenerator::new();
+            let mut move_list = MoveList::new();
 
-        let depth = 2;
+            let mut total_nodes: i32 = 0;
 
-        let mut perft_result: HashMap<String, i32> = Default::default();
+            let depth = 1;
 
-        let elapsed_time = measure_time(|| {
-            let _ = move_generator.generate_moves(&board, &mut move_list, MoveType::All);
-            for mov in move_list.moves.iter() {
-                if mov.data > 0 {
-                    board.make_move(*mov, &move_generator);
-                    let the_move = format!("{}{}", SQUARE_NAME[mov.from()], SQUARE_NAME[mov.to()]);
-                    let nodes = perft_results(depth, &mut board, &move_generator, &the_move);
-                    total_nodes += nodes;
-                    perft_result.insert(the_move, nodes);
-                    board.unmake();
+            let mut perft_result: HashMap<String, i32> = Default::default();
+
+            let elapsed_time = measure_time(|| {
+                let _ = move_generator.generate_moves(&board, &mut move_list, MoveType::All);
+                for mov in move_list.moves.iter() {
+                    if mov.data > 0 {
+                        board.make_move(*mov, &move_generator);
+                        let the_move =
+                            format!("{}{}", SQUARE_NAME[mov.from()], SQUARE_NAME[mov.to()]);
+                        let nodes = perft_results(depth, &mut board, &move_generator, &the_move);
+                        total_nodes += nodes;
+                        perft_result.insert(the_move, nodes);
+                        board.unmake();
+                    }
                 }
-            }
-        });
+            });
 
-        let mut sorted_vec: Vec<_> = perft_result.into_iter().collect();
+            // let mut sorted_vec: Vec<_> = perft_result.into_iter().collect();
 
-        // Sort the vector based on the keys
-        sorted_vec.sort_by(|a, b| a.0.cmp(&b.0));
+            // Sort the vector based on the keys
+            // sorted_vec.sort_by(|a, b| a.0.cmp(&b.0));
 
-        // Iterate over the sorted vector
-        for (key, value) in sorted_vec {
-            println!("{}: {}", key, value);
+            // // Iterate over the sorted vector
+            // for (key, value) in sorted_vec {
+            //     println!("{}: {}", key, value);
+            // }
+
+            println!("Position: {}\n", position);
+
+            println!(
+                "Depth: {} ply     Result: {} nodes    Time: {:?} milliseconds\n",
+                depth + 1,
+                total_nodes,
+                elapsed_time.as_millis()
+            );
+
+            println!(
+                "---------------------------------------------------------------------------\n"
+            );
         }
-
-        println!(
-            "Depth: {} ply     Result: {} positions    Time: {:?} milliseconds",
-            depth,
-            total_nodes,
-            elapsed_time.as_millis()
-        );
     }
 }
 
@@ -436,9 +453,6 @@ fn perft_results(
     for mov in move_list.moves.iter() {
         if mov.data > 0 {
             if board.make_move(*mov, move_generator) {
-                if current_move == "e2e4" {
-                    println!("{}{}", SQUARE_NAME[mov.from()], SQUARE_NAME[mov.to()]);
-                }
                 let nodes = perft_results(depth - 1, board, &move_generator, current_move);
                 total_nodes += nodes;
                 board.unmake();
